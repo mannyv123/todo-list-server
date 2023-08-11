5deb34eea5c65423d0c48b6a35ce0a0587654547
Manjot Virdi <virdi_manjot@hotmail.com>
2023-08-10 23:20:46 -0700

    set up db connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,30 @@
 import express, { Express } from "express";
+import mongoose from "mongoose";
+import tasksRouter from "./tasksRouter";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 const PORT = process.env.PORT || 3001;
+const MONGO = process.env.MONGO;
 const app: Express = express();
 
-import tasksRouter from "./tasksRouter";
-app.use("/tasks", tasksRouter);
+async function startServer() {
+    if (!MONGO) {
+        throw new Error("No MongoDB connection url provided.");
+    }
 
-app.listen(PORT, () => {
-    console.log(`Express server listening on port: ${PORT}`);
-});
+    try {
+        await mongoose.connect(MONGO);
+        console.log("Connected to MongoDB");
+
+        app.listen(PORT, () => {
+            console.log(`Express server listening on port: ${PORT}`);
+        });
+    } catch (err) {
+        console.error(`Error connecting to MongoDB: ${err}`);
+    }
+}
+
+startServer();
+
+app.use("/tasks", tasksRouter);
